4197bfb6bc117454d1eab9835767c0e5743723d6
Abdullrahman Al-Akhzami <96819774+AAkhzami@users.noreply.github.com>
2026-01-17 18:58:59 +0400

    Add files via upload

---
+++ Password_Manager_Data_Layer/clsDataAccessSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Data_Layer
+{
+    public class clsDataAccessSettings
+    {
+
+        public static string connectionString = "Server = .; Database = PasswordManagerDB; User Id = YourUserID; Password = YourPassword";
+    }
+}
